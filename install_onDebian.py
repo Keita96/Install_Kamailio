bb04754c3862e856bd56e9767179c0ac0966028f
Samba KEITA <47242081+Keita96@users.noreply.github.com>
2021-02-10 17:05:59 +0000

    Update install_onDebian.py

--- install_onDebian.py
+++ install_onDebian.py
@@ -1,3 +1,9 @@
 wget -O- http://deb.kamailio.org/kamailiodebkey.gpg | sudo apt-key add -
 echo "deb http://deb.kamailio.org/kamailio51 $(lsb_release -sc) main" >/etc/apt/sources.list.d/kamailio.list
 apt update && apt install kamailio kamailio-mysql-modules kamailio-presence-modules kamailio-xml-modules
+
+
+
+###to gerate key 
+
+https://lmtools.com/node/236
